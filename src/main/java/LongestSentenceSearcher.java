446c8075b6a9cc8a132a9d3f1fa6b007191e7dde
Boris Solomonia <borissolomoniaphone@gmail.com>
2022-07-14 10:50:39 +0400

    first commit of the longest sentence

---
+++ src/main/java/LongestSentenceSearcher.java
@@ -0,0 +1,20 @@
+public class LongestSentenceSearcher {
+    public static int longestSentenceSize(String text){
+        return wordsCounter(textSplitter(text));
+    }
+
+    private static String[] textSplitter(String firstText){
+        String[] a = firstText.split("[!?.]+");
+        return a;
+    }
+    private static int wordsCounter(String[] sentences){
+        String[] wordsOfSentence;
+        int wordsAmount = 0;
+        for (String a: sentences) {
+
+            wordsOfSentence = a.trim().split("\\s+");
+            if(wordsOfSentence.length > wordsAmount) wordsAmount = wordsOfSentence.length;
+        }
+        return wordsAmount;
+    }
+}
